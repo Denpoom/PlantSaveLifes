0554f4ae6c526fe572eeb62ef0e87f00fa8a84f0
sahussawud <sahussawud@hotmail.com>
2019-04-21 18:11:04 +0700

    Complete PlantSaveLife
    Sunday, April 21 2019

--- plantsavelife.ino
+++ plantsavelife.ino
@@ -6,11 +6,28 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);//declare for setu
 //define function(); 
 int soilconvert(int soil_moisture); // measuring humidity of soil by soil moisture senser V1.2
 
-int airquility(){}; // checking air quility for SHARP GP2Y10 Dust Sensor PM2.5 module.
+void airquility(); // checking air quility for SHARP GP2Y10 Dust Sensor PM2.5 module.
+
+// Arduino pin numbers.///////////////////////////////////////////////////////////
+const int sharpLEDPin = 7;   // Arduino digital pin 7 connect to sensor LED.
+const int sharpVoPin = A2;   // Arduino analog pin 5 connect to sensor Vo.
+
+#define N 100
+static unsigned long VoRawTotal = 0;
+static int VoRawCount = 0;
+
+// Set the typical output voltage in Volts when there is zero dust. 
+static float Voc = 0.6;
+
+// Use the typical sensitivity in units of V per 100ug/m3.
+const float K = 0.5;
+///////////////////////////////////////////////////////////////////////////////////
 
 float pumpactive_duration;
 
-int pump();
+bool first_cycle = true;// Defaut First Cycle of loop() there are gap while waiting for First-Cycle of loop()
+
+int pump();//declare pump() for command pump to work.
 
 void setup(){
     Serial.begin(9600);
@@ -18,6 +35,7 @@ void setup(){
     pinMode(2, OUTPUT);//RELAY CONTROL PUMP 2-5v
     pinMode(A1, INPUT);//Registor adjustable
     pinMode(A0, INPUT);//SOIL MOISURE MEANSUREMENT V1.2
+    pinMode(sharpLEDPin, OUTPUT); // Set LED pin for SHARP GP2Y10 Dust Sensor PM2.5 module.
 
 
     //BIOS MODE FOR DELEY PROGRAM;
@@ -70,15 +88,82 @@ void setup(){
     
 }
 void loop() // While (true){}
-{
+{ 
+  
+  if(first_cycle){
+     
+  lcd.setCursor(0,0);
+  lcd.print("CHECKING SENSOR");
+  lcd.setCursor(0,1);
+  lcd.print("status:");
+  for(int i = 1; i < 9;i++){
+    lcd.print("#");
+    delay(600);
+  }
+  lcd.setCursor(0,1);
+  lcd.print("status: Connect");
+  delay(2000);
+  first_cycle = false; // end of first Cycle preload
+  
+  }
+  
+  digitalWrite(sharpLEDPin, LOW);
+
+  // Wait 0.28ms before taking a reading of the output voltage as per spec.
+  delayMicroseconds(280);
+
+  // Record the output voltage. This operation takes around 100 microseconds.
+  int VoRaw = analogRead(sharpVoPin);
+  
+  // Turn the dust sensor LED off by setting digital pin HIGH.
+  digitalWrite(sharpLEDPin, HIGH);
+
+  // Wait for remainder of the 10ms cycle = 10000 - 280 - 100 microseconds.
+  delayMicroseconds(9620);
+  
+  
+  // Use averaging 100 value
+  float Vo = VoRaw;
+  VoRawTotal += VoRaw;
+  VoRawCount++;
+  if ( VoRawCount >= N ) {
+    Vo = 1.0 * VoRawTotal / N;
+    VoRawCount = 0;
+    VoRawTotal = 0;
+  } else {
+    return;
+  }
+
+  // Compute the output voltage in Volts.
+  Vo = Vo / 1024.0 * 5.0;
+
+  // Convert to Dust Density in units of ug/m3.
+  float dV = Vo - Voc;
+  if ( dV < 0 ) {
+    dV = 0;
+    Voc = Vo;
+  }
+  float dustDensity = dV / K * 100.0;
 
   int moisture = soilconvert(analogRead(A0)); // convert analog value to percent;
+
+  lcd.clear();//Clear Screen immediately when new value come it has delay around 10000 millisecond
+
 
   lcd.setCursor(0,0);
   lcd.print("Moisture: ");
   lcd.print(moisture);
   lcd.print("%");
-
+  
+  ////////////////////////////////////////////
+    // Turn on the dust sensor LED by setting digital pin LOW.
+  lcd.setCursor(0,1);
+  lcd.print("Dust:");
+  lcd.print(dustDensity);
+  lcd.print("ug/m3");
+  
+  ////////////////////////////////////////////
+  
   //display in computer
   Serial.println("Moisture:");
   Serial.println(moisture);
@@ -123,9 +208,7 @@ void loop() // While (true){}
         }
       }  
     }
-  }
-  lcd.clear();
-  
+  }  
 
 }
 
@@ -146,8 +229,5 @@ int pump(){
   delay(2000);
   digitalWrite(2, LOW);
   return 2;
-}
-int airquility(){
-  
   
 }
